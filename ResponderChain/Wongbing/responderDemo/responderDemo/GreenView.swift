16925b18e7dbb7c4314e4cd777f320c3ef6906d3
웡빙 <wb277@naver.com>
2022-07-11 18:48:04 +0900

    feat: responderChain 미해결

---
+++ ResponderChain/Wongbing/responderDemo/responderDemo/GreenView.swift
@@ -0,0 +1,32 @@
+//
+//  GreenView.swift
+//  responderDemo
+//
+//  Created by 이원빈 on 2022/07/09.
+//
+
+import UIKit
+
+class GreenView: UIView {
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if !isUserInteractionEnabled || isHidden || alpha <= 0.01 {
+            return nil
+        }
+     
+        if self.point(inside: point, with: event) {
+            for subview in subviews.reversed() {
+                let convertedPoint = subview.convert(point, from: self)
+                if let hitTestView = subview.hitTest(convertedPoint, with: event) {
+                    print(hitTestView.restorationIdentifier!)
+                    return hitTestView
+                }
+            }
+            print(self.restorationIdentifier!)
+            return self
+        }
+        print("isNotGreenView")
+
+        return nil
+    }
+}
---
+++ ResponderChain/Wongbing/responderDemo/responderDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  responderDemo
+//
+//  Created by 이원빈 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
